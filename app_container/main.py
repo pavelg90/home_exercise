51ee75a10781915145aaacd17cb5042d5d74df94
pavel_yael <pavelg@yaelgroup.com>
2023-05-21 00:11:01 +0300

    small fix

--- app_container/main.py
+++ app_container/main.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from io import StringIO
 
 # Set up MinIO client
-s3 = boto3.client('s3', endpoint_url='http://s3_simulator:9000',
+s3 = boto3.client('s3',
+                  endpoint_url='http://localhost:333',
                   aws_access_key_id='minio',
                   aws_secret_access_key='minio123')
 
@@ -15,12 +16,15 @@ engine = create_engine('postgresql://admin:admin@db:5432/')
 # Download CSV files from MinIO and load them into the PostgreSQL database
 def load_csvs_from_minio_to_db():
     for obj in s3.list_objects(Bucket='mybucket')['Contents']:
-        filename = obj['Key']
-        csv_obj = s3.get_object(Bucket='mybucket', Key=filename)
-        body = csv_obj['Body'].read().decode('utf-8')
-        df = pd.read_csv(StringIO(body))
-        table_name = os.path.splitext(filename)[0]  # Use the CSV filename (without extension) as the table name
-        df.to_sql(table_name, engine, if_exists='replace')
+        try:
+            filename = obj['Key']
+            csv_obj = s3.get_object(Bucket='mybucket', Key=filename)
+            body = csv_obj['Body'].read().decode('utf-8')
+            df = pd.read_csv(StringIO(body))
+            table_name = os.path.splitext(filename)[0]  # Use the CSV filename (without extension) as the table name
+            df.to_sql(table_name, engine, if_exists='replace')
+        except:
+            print('Something bad happened, or not...')
 
 if __name__ == "__main__":
     load_csvs_from_minio_to_db()
