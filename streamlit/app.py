8d37de8892e0dca6444c76a0be8dd8755bb65516
pavel_yael <pavelg@yaelgroup.com>
2023-05-21 00:18:04 +0300

    small fix

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,7 +4,7 @@ import os
 
 # Setup Minio connection
 s3 = boto3.client('s3',
-                  endpoint_url='http://localhost:333',
+                  endpoint_url='http://s3_simulator:9000',
                   aws_access_key_id='minio',
                   aws_secret_access_key='minio123')
 
