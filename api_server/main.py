13b15ad0ca2b9ece6a81a0e958d793a14d6ea9ff
pavel_yael <pavelg@yaelgroup.com>
2023-05-20 23:14:52 +0300

    small fix

--- api_server/main.py
+++ api_server/main.py
@@ -1,4 +1,3 @@
-import time
 from fastapi import FastAPI
 from sqlalchemy import create_engine, exc
 
@@ -9,9 +8,10 @@ while not database_initialized:
     try:
         engine = create_engine('postgresql://admin:admin@db:5432/dbname')
         with engine.connect() as connection:
-            # Try a simple query to check if connection is established
-            connection.execute("SELECT 1")
-            database_initialized = True
+            with connection.begin():
+                # Try a simple query to check if connection is established
+                connection.execute("SELECT 1")
+                database_initialized = True
     except exc.DBAPIError as e:
         print("Database not ready yet, waiting...")
         time.sleep(5)
@@ -23,6 +23,7 @@ def read_root():
 @app.get("/{id}")
 def read_item(id: int):
     with engine.connect() as connection:
-        result = connection.execute(f"SELECT * FROM users WHERE ID = {id}")
-        row = result.fetchone()
-        return {"id": row[0], "name": row[1]} if row else {"message": "User not found"}
+        with connection.begin():
+            result = connection.execute(f"SELECT * FROM users WHERE ID = {id}")
+            row = result.fetchone()
+            return {"id": row[0], "name": row[1]} if row else {"message": "User not found"}
