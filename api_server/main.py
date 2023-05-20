e5b681ed5d7e14a5a10e1d164d8c6b2184707bfb
pavel_yael <pavelg@yaelgroup.com>
2023-05-20 22:56:34 +0300

    Fixed api_server/main

--- api_server/main.py
+++ api_server/main.py
@@ -5,8 +5,13 @@ app = FastAPI()
 
 engine = create_engine('postgresql://admin:admin@db:5432/dbname')
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI server is running"}
+
 @app.get("/{id}")
 def read_item(id: int):
     with engine.connect() as connection:
         result = connection.execute(f"SELECT * FROM users WHERE ID = {id}")
-        return result.fetchone()
+        row = result.fetchone()
+        return {"id": row[0], "name": row[1]} if row else {"message": "User not found"}
