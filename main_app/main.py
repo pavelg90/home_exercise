0c9b6843bb9c75cbbb6e78c94168ba5ece1370b5
pavel_yael <pavelg@yaelgroup.com>
2023-05-21 13:26:52 +0300

    debugging app_container

--- main_app/main.py
+++ main_app/main.py
@@ -1,7 +1,36 @@
+from minio import Minio
+from minio.error import S3Error
 import time
+
+
+def main():
+    # Create a client with the MinIO server playground, its access key
+    # and secret key.
+    client = Minio(
+        "s3_simulator",
+        access_key="minio",
+        secret_key="minio123",
+    )
+
+    # Make 'mybucket' bucket if not exist.
+    if not client.bucket_exists("mybucket"):
+        client.make_bucket("asiatrip")
+    else:
+        print("Bucket 'asiatrip' already exists")
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error occurred.", exc)
 
 if __name__ == "__main__":
     while True:
-        a=1
+        try:
+            main()
+        except Exception as e:
+            print("error occurred.", e)
+
         time.sleep(1)
-        print('OK')
